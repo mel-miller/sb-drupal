0fd456d98bbd56c1769ce7fa1cd4cc26a59adff4
M Miller <melissa.miller@pantheon.io>
2022-09-10 11:44:01 -0700

    install and enable storybook/addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,10 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
   framework: '@storybook/html',
 };
