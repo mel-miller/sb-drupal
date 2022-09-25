7c2ac04480b0cc334c5176ec2bf3479a4a227883
M Miller <melissa.miller@pantheon.io>
2022-09-25 14:18:00 -0700

    Update main.js
    disable Storybook interactions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
   framework: '@storybook/html',
   staticDirs: ['../public'],
 };
