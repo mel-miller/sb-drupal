fb007511a37877479452e3c865d29b1a97f41dc6
M Miller <melissa.miller@pantheon.io>
2022-09-25 15:44:37 -0700

    Add anchor component; update others; add color vars

---
+++ src/foundations/anchor/anchor.stories.js
@@ -0,0 +1,27 @@
+import TwigAnchor from './anchor.local.twig';
+import AnchorDocs from '!!raw-loader!./anchor.docs.mdx';
+
+export default {
+  title: 'Foundations/Typography/Anchor',
+  parameters: {
+    componentSubtitle: 'HTML anchor element',
+    docs: {
+      description: {
+        component: AnchorDocs,
+      },
+    },
+  },
+  argTypes: {
+    href: {
+      description: 'Link href',
+    },
+    text: {
+      description: 'Link text',
+    },
+  },
+  args: { href: '#', text: 'this is an anchor link' },
+};
+
+const Template = ({ href, text }) => TwigAnchor({ href, text });
+
+export const Anchor = Template.bind({});
