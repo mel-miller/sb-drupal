9bfaac8e19baaa3a4f806a28973ef2a2d6513e42
M Miller <melissa.miller@pantheon.io>
2022-09-27 17:52:46 -0700

    Rework button; update neutrals

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,47 +1,64 @@
-import TwigButton from './button.twig';
-import ButtonDocs from '!!raw-loader!./button.docs.mdx';
+import TwigButton from './button.twig'
+import ButtonDocs from '!!raw-loader!./button.docs.mdx'
 
 export default {
-  title: 'Components/Button',
-  parameters: {
-    componentSubtitle: 'An interactive element used to initiate an action',
-    docs: {
-      description: {
-        component: ButtonDocs,
-      },
-    },
-  },
-  argTypes: {
-    text: {
-      description: 'Button text',
-    },
-    variant: {
-      description: 'Button variant',
-      table: { defaultValue: { summary: 'primary' } },
-      control: {
-        type: 'radio',
-        options: ['primary', 'secondary'],
-      },
-    },
-    type: {
-      description: 'Button type attribute',
-      table: { defaultValue: { summary: 'button' } },
-      control: {
-        type: 'radio',
-        options: ['button', 'submit', 'reset'],
-      },
-    },
-    disabled: {
-      description: 'Button is disabled',
-      table: { defaultValue: { summary: 'false' } },
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
-  args: { text: 'Button', variant: 'primary', type: 'button', disabled: false },
-};
+	title: 'Components/Button',
+	parameters: {
+		componentSubtitle: 'An interactive element used to initiate an action',
+		docs: {
+			description: {
+				component: ButtonDocs,
+			},
+		},
+	},
+	argTypes: {
+		text: {
+			description: 'Button text',
+		},
+		variant: {
+			description: 'Button variant',
+			table: { defaultValue: { summary: 'primary' } },
+			control: {
+				type: 'radio',
+				options: ['primary', 'secondary', 'reverse'],
+			},
+		},
+		type: {
+			description: 'Button type attribute',
+			table: { defaultValue: { summary: 'button' } },
+			control: {
+				type: 'radio',
+				options: ['button', 'submit', 'reset'],
+			},
+		},
+		disabled: {
+			description: 'Button is disabled',
+			table: { defaultValue: { summary: 'false' } },
+			control: {
+				type: 'boolean',
+			},
+		},
+	},
+	args: { text: 'Button', variant: 'primary', type: 'button', disabled: false },
+}
 
-const Template = ({ text, variant, type, disabled }) => TwigButton({ text, variant, type, disabled });
+const Template = ({ text, variant, type, disabled }) =>
+	TwigButton({ text, variant, type, disabled })
 
-export const Button = Template.bind({});
+export const ButtonDefault = Template.bind({})
+
+export const ButtonSecondary = Template.bind({})
+ButtonSecondary.args = {
+	variant: 'secondary',
+}
+
+export const ButtonReverse = Template.bind({})
+ButtonReverse.args = {
+	variant: 'reverse',
+}
+ButtonReverse.parameters = {
+	backgrounds: { default: 'dark' },
+	docs: {
+		storyDescription: 'To be used on a dark-colored background.',
+	},
+}
