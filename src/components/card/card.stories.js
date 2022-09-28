d53ffe64003d4496c441e665c666f934401d8ecd
M Miller <melissa.miller@pantheon.io>
2022-09-27 20:36:35 -0700

    rough-in card component

---
+++ src/components/card/card.stories.js
@@ -0,0 +1,82 @@
+import TwigCard from './card.twig'
+import CardDocs from '!!raw-loader!./card.docs.mdx'
+
+export default {
+	title: 'Components/Card',
+	parameters: {
+		componentSubtitle:
+			'A container to group related information, often used as a teaser',
+		docs: {
+			description: {
+				component: CardDocs,
+			},
+		},
+	},
+	argTypes: {
+		element_type: {
+			description: 'Base HTML element for card — `div`, `article`, or `aside`.',
+			table: { defaultValue: { summary: 'div' } },
+			options: ['div', 'article', 'aside'],
+			control: {
+				type: 'select',
+			},
+		},
+		image: {
+			description: 'Card image.',
+		},
+		kicker_text: {
+			description: 'Kicker. Can also be used as a category or date field.',
+		},
+		heading_text: {
+			description: 'Card heading.',
+		},
+		heading_level: {
+			description: 'HTML element for heading text — `h2` or `h3`',
+			table: { defaultValue: { summary: 'h2' } },
+			options: ['h2', 'h3'],
+			control: {
+				type: 'select',
+			},
+		},
+		body_text: {
+			description: 'Short description or summary.',
+		},
+		link: {
+			description: 'Card CTA.',
+		},
+	},
+	args: {
+		element_type: 'div',
+		image: {
+			src: 'demo-images/placeimg_360_240_nature.jpeg',
+			alt: 'Beach at sunset.',
+		},
+		kicker_text: 'Category Name',
+		heading_text: 'This is the card heading',
+		heading_level: 'h2',
+		body_text:
+			'Body text of content, or content summary. Helvetica kombucha shabby chic, mustache put a bird on it. Green juice twee iceland cardigan retro art party lomo intelligentsia.',
+		link: { text: 'Read More', url: '#' },
+	},
+}
+
+const Template = ({
+	element_type,
+	image,
+	kicker_text,
+	heading_text,
+	heading_level,
+	body_text,
+	link,
+}) =>
+	TwigCard({
+		element_type,
+		image,
+		kicker_text,
+		heading_text,
+		heading_level,
+		body_text,
+		link,
+	})
+
+export const Card = Template.bind({})
