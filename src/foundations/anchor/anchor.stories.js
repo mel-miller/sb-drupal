e4b8bd8edd18ddfe1b3361e0c01661a707390a41
M Miller <melissa.miller@pantheon.io>
2022-10-02 16:39:10 -0700

    re-org foundations display

--- src/foundations/anchor/anchor.stories.js
+++ src/foundations/anchor/anchor.stories.js
@@ -1,27 +1,27 @@
-import TwigAnchor from './anchor.local.twig';
-import AnchorDocs from '!!raw-loader!./anchor.docs.mdx';
+import TwigAnchor from './anchor.local.twig'
+import AnchorDocs from '!!raw-loader!./anchor.docs.mdx'
 
 export default {
-  title: 'Foundations/Typography/Anchor',
-  parameters: {
-    componentSubtitle: 'HTML anchor element',
-    docs: {
-      description: {
-        component: AnchorDocs,
-      },
-    },
-  },
-  argTypes: {
-    href: {
-      description: 'Link href',
-    },
-    text: {
-      description: 'Link text',
-    },
-  },
-  args: { href: '#', text: 'this is an anchor link' },
-};
+	title: 'Foundations/Anchor',
+	parameters: {
+		componentSubtitle: 'HTML anchor element',
+		docs: {
+			description: {
+				component: AnchorDocs,
+			},
+		},
+	},
+	argTypes: {
+		href: {
+			description: 'Link href',
+		},
+		text: {
+			description: 'Link text',
+		},
+	},
+	args: { href: '#', text: 'this is an anchor link' },
+}
 
-const Template = ({ href, text }) => TwigAnchor({ href, text });
+const Template = ({ href, text }) => TwigAnchor({ href, text })
 
-export const Anchor = Template.bind({});
+export const Anchor = Template.bind({})
