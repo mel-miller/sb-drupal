049da57572c67a57777979ce0dd8461ef749d8d5
M Miller <melissa.miller@pantheon.io>
2022-10-01 17:33:37 -0700

    add storybook-source-code-addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,11 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: '@storybook/html',
-  staticDirs: ['../public'],
-};
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-a11y',
+		'storybook-source-code-addon',
+	],
+	framework: '@storybook/html',
+	staticDirs: ['../public'],
+}
