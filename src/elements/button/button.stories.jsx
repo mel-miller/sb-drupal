883cf9b997256b870dad5358703a96f6bf6348ab
Melissa Miller <melissa.miller@pantheon.io>
2025-09-21 17:26:21 -0700

    Start button element

---
+++ src/elements/button/button.stories.jsx
@@ -0,0 +1,23 @@
+import markup from './button.twig'
+import data from './button.yml'
+
+export default {
+	title: 'Elements/Button',
+	tags: ['autodocs'],
+	render: (args) => markup(args),
+	argTypes: {
+		variant: {
+			control: { type: 'select' },
+			options: ['primary', 'secondary', 'critical'],
+		},
+		type: {
+			control: { type: 'select' },
+			options: ['button', 'submit', 'reset'],
+		},
+	},
+	args: {
+		...data,
+	},
+}
+
+export const Default = {}
